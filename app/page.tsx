b7a392a91e5104ac15841ce39e63c820184fa57f
Khuleg <26bs38301nest.edu.mn@G301-CC0210.local>
2026-06-19 11:25:22 +0800

    homedashboard ru userdeg bolgov

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import HomeDashboard from "./home-dashboard/page";
+
 export default function Home() {
-  return <div></div>;
+  redirect("/home-dashboard");
 }
